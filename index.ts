d4714adb14af6809c32630d5c889cee3f0349dc3
jnavarro <21sebas12@gmail.com>
2022-07-18 10:09:33 -0500

    fix: :adhesive_bandage: the recommendations made are fixed.

--- index.ts
+++ index.ts
@@ -1,67 +1,67 @@
-import { useRef, useState, useEffect, RefObject } from "react";
+import { useRef, useState, useEffect, RefObject } from 'react';
 
-import Quill, { QuillOptionsStatic } from "quill";
+import Quill, { QuillOptionsStatic } from 'quill';
 
-const theme = "snow";
+const theme = 'snow';
 
 const modules = {
-  toolbar: [
-    ["bold", "italic", "underline", "strike"],
-    [{ align: [] }],
+    toolbar: [
+        ['bold', 'italic', 'underline', 'strike'],
+        [{ align: [] }],
 
-    [{ list: "ordered" }, { list: "bullet" }],
-    [{ indent: "-1" }, { indent: "+1" }],
+        [{ list: 'ordered' }, { list: 'bullet' }],
+        [{ indent: '-1' }, { indent: '+1' }],
 
-    [{ size: ["small", false, "large", "huge"] }],
-    [{ header: [1, 2, 3, 4, 5, 6, false] }],
-    ["link", "image", "video"],
-    [{ color: [] }, { background: [] }],
+        [{ size: ['small', false, 'large', 'huge'] }],
+        [{ header: [1, 2, 3, 4, 5, 6, false] }],
+        ['link', 'image', 'video'],
+        [{ color: [] }, { background: [] }],
 
-    ["clean"],
-  ],
-  clipboard: {
-    matchVisual: false,
-  },
+        ['clean'],
+    ],
+    clipboard: {
+        matchVisual: false,
+    },
 };
 
 const formats = [
-  "bold",
-  "italic",
-  "underline",
-  "strike",
-  "align",
-  "list",
-  "indent",
-  "size",
-  "header",
-  "link",
-  "image",
-  "video",
-  "color",
-  "background",
-  "clean",
+    'bold',
+    'italic',
+    'underline',
+    'strike',
+    'align',
+    'list',
+    'indent',
+    'size',
+    'header',
+    'link',
+    'image',
+    'video',
+    'color',
+    'background',
+    'clean',
 ];
 
 function assign(target: any, _varArgs: any) {
-  "use strict";
-  if (target === null || target === undefined) {
-    throw new TypeError("Cannot convert undefined or null to object");
-  }
-
-  const to = Object(target);
-
-  for (let index = 1; index < arguments.length; index++) {
-    const nextSource = arguments[index];
-
-    if (nextSource !== null && nextSource !== undefined) {
-      for (const nextKey in nextSource) {
-        if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
-          to[nextKey] = nextSource[nextKey];
-        }
-      }
+    'use strict';
+    if (target === null || target === undefined) {
+        throw new TypeError('Cannot convert undefined or null to object');
     }
-  }
-  return to;
+
+    const to = Object(target);
+
+    for (let index = 1; index < arguments.length; index++) {
+        const nextSource = arguments[index];
+
+        if (nextSource !== null && nextSource !== undefined) {
+            for (const nextKey in nextSource) {
+                if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+                    to[nextKey] = nextSource[nextKey];
+                }
+            }
+        }
+    }
+    return to;
 }
 
 /**
@@ -69,37 +69,35 @@ function assign(target: any, _varArgs: any) {
  * @param options Quill static options. https://github.com/gtgalone/react-quilljs#options
  * @returns Returns quill, quillRef, and Quill. https://github.com/gtgalone/react-quilljs#return
  */
-export const useQuill = (
-  options: QuillOptionsStatic | undefined = { theme, modules, formats }
-) => {
-  const quillRef: RefObject<any> = useRef();
+export const useQuill = (options: QuillOptionsStatic | undefined = { theme, modules, formats }) => {
+    const quillRef: RefObject<any> = useRef();
 
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [obj, setObj] = useState({
-    Quill: undefined as any | undefined,
-    quillRef,
-    quill: undefined as Quill | undefined,
-    editorRef: quillRef,
-    editor: undefined as Quill | undefined,
-  });
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [obj, setObj] = useState({
+        Quill: undefined as any | undefined,
+        quillRef,
+        quill: undefined as Quill | undefined,
+        editorRef: quillRef,
+        editor: undefined as Quill | undefined,
+    });
 
-  useEffect(() => {
-    if (!obj.Quill) {
-      setObj((prev) => ({ ...prev, Quill }));
-    }
-    if (obj.Quill && !obj.quill && quillRef && quillRef.current && isLoaded) {
-      const opts = {
-        ...options,
-        modules: assign(modules, options.modules),
-        formats: options.formats || formats,
-        theme: options.theme || theme,
-      };
-      const quill = new obj.Quill(quillRef.current, opts);
+    useEffect(() => {
+        if (!obj.Quill) {
+            setObj(prev => ({ ...prev, Quill }));
+        }
+        if (obj.Quill && !obj.quill && quillRef && quillRef.current && isLoaded) {
+            const opts = {
+                ...options,
+                modules: assign(modules, options.modules),
+                formats: options.formats || formats,
+                theme: options.theme || theme,
+            };
+            const quill = new obj.Quill(quillRef.current, opts);
 
-      setObj((prev) => ({ ...prev, quill, editor: quill }));
-    }
-    setIsLoaded(true);
-  }, [isLoaded, obj, options]);
+            setObj(assign(assign({}, obj), { quill, editor: quill }));
+        }
+        setIsLoaded(true);
+    }, [isLoaded, obj, options]);
 
-  return obj;
+    return obj;
 };
